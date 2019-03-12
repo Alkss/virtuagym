7db0449c590cc002bbb02de841a6d6ecd32c2aef
Alkss <alkss_fjv@yahoo.com.br>
2019-03-11 23:57:52 -0300

    work in progress: adding new workout plan

--- control/js/exercises-list.js
+++ control/js/exercises-list.js
@@ -3,6 +3,7 @@ var exerciseNumber = 1;
 
 $(document).ready(function () {
     refresh();
+    checkAddDayBtn();
 
     //Add a new exercise to an existing day
     $('.addExerciseBtn').click(function () {
@@ -12,6 +13,7 @@ $(document).ready(function () {
     //Add a new day of exercise to an existing planing day
     $('.addNewDayOfExercise').click(function () {
         addDayOfExercise();
+        checkAddDayBtn();
     });
 
     //Remove the selected exercise from an existing day
@@ -19,6 +21,15 @@ $(document).ready(function () {
         if (confirm('Are you sure?')) {
             var entryDay = $(this).attr('data-exercise_day');
             $("#exerciseNumber_" + entryDay).remove();
+        }
+    });
+
+    //Remove the selected day of exercise from an existing planing day
+    $(document).on('click', '.removeDayBtn', function () {
+        if (confirm('Are you sure?')) {
+            var entryDay = $(this).attr('data-exercise_day');
+            $("#dayOfExercise_" + entryDay).remove();
+            exerciseDay--;
         }
     });
 
@@ -40,11 +51,42 @@ function refresh() {
 
 
 function addDayOfExercise() {
+    exerciseDay++;
+    var nameOfTheDay;
+    switch (exerciseDay) {
+        case 1:
+            nameOfTheDay = "Monday";
+            break;
+        case 2:
+            nameOfTheDay = "Tuesday";
+            break;
+        case 3:
+            nameOfTheDay = "Wednesday";
+            break;
+        case 4:
+            nameOfTheDay = "Thursday";
+            break;
+        case 5:
+            nameOfTheDay = "Friday";
+            break;
+        case 6:
+            nameOfTheDay = "Saturday";
+            break;
+        case 7:
+            nameOfTheDay = "Sunday";
+            break;
+        default:
+            nameOfTheDay = "Not found";
+            break;
+    }
+
     $('.dayOfExercise').append(
-        '<div class="col-lg-12">\n' +
+        '<div class="col-lg-12" id="dayOfExercise_' + exerciseDay + '">\n' +
         '    <section class="panel">\n' +
         '        <header class="panel-heading text-center">\n' +
-        '            Monday\n' +
+        '            ' + nameOfTheDay + '\n' +
+        '<button type="button" data-exercise_day="' + exerciseDay + '" class="btn btn-danger                                                removeDayBtn">Remove\n' +
+        '                         </button>\n' +
         '        </header>\n' +
         '        <div class="addedExercises">\n' +
         '        </div>\n' +
@@ -55,7 +97,6 @@ function addDayOfExercise() {
         '                    <div class="col-sm-3">\n' +
         '                        <select class="form-control" id="exercises"\n' +
         '                                required="required">\n' +
-        '\n' +
         '                        </select>\n' +
         '                    </div>\n' +
         '                    <div class="col-sm-2">\n' +
@@ -69,6 +110,13 @@ function addDayOfExercise() {
     );
 }
 
+function checkAddDayBtn() {
+    if (exerciseDay >= 7) {
+        $('.addNewDayOfExercise').prop('disabled', true);
+    } else {
+        $('.addNewDayOfExercise').prop('disabled', false);
+    }
+}
 
 function addExerciseToDay() {
     var idExercise = $('#exercises').val();
